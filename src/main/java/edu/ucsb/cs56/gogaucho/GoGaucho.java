599d6b912e4da1772485219e4c319424ec959b45
shenmo3 <ahliu@umail.ucsb.edu>
2018-09-05 10:48:18 -0700

    Using velocity

--- src/main/java/edu/ucsb/cs56/gogaucho/GoGaucho.java
+++ src/main/java/edu/ucsb/cs56/gogaucho/GoGaucho.java
@@ -1,6 +1,9 @@
 package edu.ucsb.cs56.gogaucho;
 
 import static spark.Spark.port;
+import spark.template.velocity.VelocityTemplateEngine;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -18,8 +21,10 @@ public class GoGaucho {
 		System.out.println("");						  
 		System.out.println("In browser, visit: http://localhost:" + getHerokuAssignedPort() + "/hello");
 		System.out.println("");
-		spark.Spark.get("/", (req, res) -> "<b>Hello World!</b>\n");
-
+		spark.Spark.get("/", (req, res) -> {
+		    Map<String, Object> model = new HashMap<>();
+            return new VelocityTemplateEngine().render(new spark.ModelAndView(model, "template/hello.vm"));
+        });
 	}
 	
     static int getHerokuAssignedPort() {
