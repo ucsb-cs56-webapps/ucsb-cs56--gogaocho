e85e9d4e1d513befb9d1ed900cf176e5890459b0
Hengyu Liu <hengyuliu@umail.ucsb.edu>
2018-09-06 16:23:13 -0700

    Implement Firestore

--- src/main/java/edu/ucsb/cs56/GoGaucho/GoGauchoMain.java
+++ src/main/java/edu/ucsb/cs56/GoGaucho/GoGauchoMain.java
@@ -4,9 +4,8 @@ import static spark.Spark.port;
 
 import org.apache.log4j.Logger;
 
-
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.io.*;
 
 import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
@@ -14,27 +13,71 @@ import spark.template.mustache.MustacheTemplateEngine;
 import static spark.Spark.get;
 import static spark.Spark.post;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.*;
+
+import com.google.api.core.ApiFuture;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+
 public class GoGauchoMain {
 
 	public static final String CLASSNAME="GoGauchoMain";
-	
+	public static Firestore db;
 	public static final Logger log = Logger.getLogger(CLASSNAME);
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+
+        db = setupFirebase();
 
         port(getHerokuAssignedPort());
 		
 		Map map = new HashMap();
         map.put("name", "Sam");
 		
-        // hello.mustache file is in resources/templates directory
+
+        get("/tryDB", (rq, rs) -> {
+            tryDB();
+            return "success";
+        });
+
         get("/", (rq, rs) -> new ModelAndView(map, "intro.mustache"), new MustacheTemplateEngine());
 
 		get("/profile/user", (rq, rs) -> new ModelAndView(map, "profile.mustache"), new MustacheTemplateEngine());
 
 		post("/course/menu", (rq, rs) -> new ModelAndView(map, "course.mustache"), new MustacheTemplateEngine());
 		
-	}
+    }
+
+    static void tryDB() throws Exception {
+        DocumentReference docRef = db.collection("users").document("alovelace");
+        // Add document data  with id "alovelace" using a hashmap
+        Map<String, Object> data = new HashMap<>();
+        data.put("first", "Ada");
+        data.put("last", "Lovelace");
+        data.put("born", 1815);
+        //asynchronously write data
+        ApiFuture<WriteResult> result = docRef.set(data);
+        // ...
+        // result.get() blocks on response
+        System.out.println("Update time : " + result.get().getUpdateTime());
+    }
+    
+    static Firestore setupFirebase() throws Exception {
+
+        FileInputStream serviceAccount = new FileInputStream("src/main/java/edu/ucsb/cs56/gogaucho/ucsb-cs56-gogaucho-firebase-adminsdk-u3h8g-7d394cdd1c.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .setDatabaseUrl("https://ucsb-cs56-gogaucho.firebaseio.com")
+            .build();
+
+        FirebaseApp.initializeApp(options);
+        
+        return FirestoreClient.getFirestore();
+    }
 	
     static int getHerokuAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
@@ -43,6 +86,4 @@ public class GoGauchoMain {
         }
         return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
-
-	
 }
