c58caeeb3d0f2d3962551817288ddfdb516ea68e
shenmo3 <ahliu@umail.ucsb.edu>
2018-09-07 16:46:44 -0700

    POST for get course data, POST for adding info to DB, Courses data structure for DB

--- src/main/java/edu/ucsb/cs56/GoGaucho/GoGauchoMain.java
+++ src/main/java/edu/ucsb/cs56/GoGaucho/GoGauchoMain.java
@@ -3,13 +3,11 @@ package edu.ucsb.cs56.gogaucho;
 import static spark.Spark.port;
 import static spark.Spark.get;
 import static spark.Spark.post;
-import static spark.Spark.delete;
 
+import com.google.api.core.ApiFutures;
 import org.apache.log4j.Logger;
 
-import java.util.Iterator;
-import java.util.Map;
-import java.util.HashMap;
+import java.util.*;
 import java.io.FileInputStream;
 
 import spark.ModelAndView;
@@ -21,6 +19,7 @@ import com.google.api.core.ApiFuture;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
+import com.google.gson.Gson;
 
 public class GoGauchoMain {
 
@@ -49,30 +48,58 @@ public class GoGauchoMain {
 
 		get("/course/menu", (rq, rs) -> new ModelAndView(map, "course.mustache"), new MustacheTemplateEngine());
 
-		delete("/db/delete", ((request, response) -> {
-		    return "stub";
+		post("/api/v1/course", ((request, response) -> {
+		    if (request.queryParams().contains("Major") && request.queryParams().contains("Grade")){
+		        response.status(200);
+                String major = request.queryParams("Major");
+                String year = request.queryParams(("Grade"));
+            } else {
+                response.status(400);
+                return "Bad parameter";
+            }
+            //asynchronously retrieve all documents
+            ApiFuture<QuerySnapshot> c = db.collection("courses").document("20181")
+                    .collection("all").get();
+            List<QueryDocumentSnapshot> documents = c.get().getDocuments();
+            ArrayList<Courses> courseArr = new ArrayList<>();
+            for (QueryDocumentSnapshot document : documents) {
+                courseArr.add(document.toObject(Courses.class));
+            }
+            response.type("application/json");
+            return new Gson().toJson(courseArr);
         }));
 
-		post("/db/post", ((request, response) -> {
-            return request.queryParams();
-        }));
+		post("/api/v1/data", (request, response) -> {
+		    if (request.queryParams().contains("courseName") && request.queryParams().contains("prerequisite") &&
+            request.queryParams().contains("time") && request.queryParams().contains("date") &&
+            request.queryParams().contains("unit") && request.queryParams().contains("uid")) {
+		        response.status(200);
+		        Courses c = new Courses(request.queryParams("courseName"), request.queryParams("prerequisite"),
+                        request.queryParams("time"), Integer.parseInt(request.queryParams("date")),
+                        Integer.parseInt(request.queryParams("unit")));
+                DocumentReference docRef = db.collection("courses").document("20181")
+                        .collection("all").document(request.queryParams("uid"));
+                docRef.set(c);
+                return c;
+            } else {
+		        response.status(400);
+		        return "Bad parameter";
+            }
+        });
     }
 
-    static void tryDB() throws Exception {
-        DocumentReference docRef = db.collection("users").document("alovelace");
-        // Add document data  with id "alovelace" using a hashmap
-        Map<String, Object> data = new HashMap<>();
-        data.put("first", "Ada");
-        data.put("last", "Lovelace");
-        data.put("born", 1815);
+    private static void tryDB() throws Exception {
+	    String courseUid = "00000";
+        DocumentReference docRef = db.collection("courses").document("20181")
+                .collection("all").document(courseUid);
+        Courses c = new Courses("CS8", "N/A", "N/A", 24, 4);
         //asynchronously write data
-        ApiFuture<WriteResult> result = docRef.set(data);
-        // ...
+        ApiFuture<WriteResult> result = docRef.set(c);
         // result.get() blocks on response
         System.out.println("Update time : " + result.get().getUpdateTime());
     }
     
-    static Firestore setupFirebase() throws Exception {
+    private static Firestore setupFirebase() throws Exception {
 
         FileInputStream serviceAccount = new FileInputStream("src/main/java/edu/ucsb/cs56/gogaucho/ucsb-cs56-gogaucho-firebase-adminsdk-u3h8g-7d394cdd1c.json");
 
@@ -86,7 +113,7 @@ public class GoGauchoMain {
         return FirestoreClient.getFirestore();
     }
 	
-    static int getHerokuAssignedPort() {
+    private static int getHerokuAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
         if (processBuilder.environment().get("PORT") != null) {
             return Integer.parseInt(processBuilder.environment().get("PORT"));
