ef590bf511a026b0fc27c66b2e6d8ca1c7034a2f
Tiancheng Lin <Tiancheng@169-231-118-60.wireless.ucsb.edu>
2018-09-11 14:31:41 -0700

    Fix merged problems

--- src/main/java/edu/ucsb/cs56/GoGaucho/GoGauchoMain.java
+++ src/main/java/edu/ucsb/cs56/GoGaucho/GoGauchoMain.java
@@ -1,42 +1,19 @@
-package edu.ucsb.cs56.GoGaucho;
+package edu.ucsb.cs56.gogaucho;
 
 import static spark.Spark.port;
-<<<<<<< HEAD
-=======
 import static spark.Spark.get;
 import static spark.Spark.post;
->>>>>>> e660f884501f8a4934033de6354c0521822e9af4
 
 import com.google.api.core.ApiFutures;
 import com.google.gson.GsonBuilder;
 import org.apache.log4j.Logger;
 
-<<<<<<< HEAD
-
-import java.util.HashMap;
-import java.util.Map;
-=======
 import java.util.*;
 import java.io.FileInputStream;
->>>>>>> e660f884501f8a4934033de6354c0521822e9af4
 
 import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
 
-<<<<<<< HEAD
-import static spark.Spark.get;
-import static spark.Spark.post;
-
-/**
- * Simple example of using Mustache Templates
- *
- */
-
-public class GoGauchoMain {
-
-	public static final String CLASSNAME="GoGaucho";
-	
-=======
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.*;
 import com.google.api.core.ApiFuture;
@@ -47,45 +24,37 @@ import com.google.gson.Gson;
 
 public class GoGauchoMain {
 
-	private static final String CLASSNAME="GoGauchoMain";
-	private static Firestore db;
->>>>>>> e660f884501f8a4934033de6354c0521822e9af4
-	public static final Logger log = Logger.getLogger(CLASSNAME);
+    private static final String CLASSNAME="GoGauchoMain";
+    private static Firestore db;
+    public static final Logger log = Logger.getLogger(CLASSNAME);
 
-	public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        db = setupFirebase();
 
         port(getHerokuAssignedPort());
-		
-		Map map = new HashMap();
+        
+        Map map = new HashMap();
         map.put("name", "Sam");
-<<<<<<< HEAD
-		
-        // hello.mustache file is in resources/templates directory
-        get("/", (rq, rs) -> new ModelAndView(map, "intro.mustache"), new MustacheTemplateEngine());
-
-		get("/personalinfo", (rq, rs) -> new ModelAndView(map, "personalinfo.mustache"), new MustacheTemplateEngine());
-
-		get("/course", (rq, rs) -> new ModelAndView(map, "course.mustache"), new MustacheTemplateEngine());
-=======
 
         // Front end template render
         get("/", (rq, rs) -> new ModelAndView(map, "intro.mustache"), new MustacheTemplateEngine());
 
-		get("/profile/user", (rq, rs) -> new ModelAndView(map, "profile.mustache"), new MustacheTemplateEngine());
+        get("/profile/user", (rq, rs) -> new ModelAndView(map, "profile.mustache"), new MustacheTemplateEngine());
 
-		get("/course/menu", (rq, rs) -> new ModelAndView(map, "course.mustache"), new MustacheTemplateEngine());
+        get("/course/menu", (rq, rs) -> new ModelAndView(map, "course.mustache"), new MustacheTemplateEngine());
 
-		/*
-		Get all course from DB:
-		    Method: POST
-		    Param:  Major, Grade
-		    Return: 400 if param missing
-		            200 if success
-		 */
-		post("/api/v1/getcourse", ((request, response) -> {
+        /*
+        Get all course from DB:
+            Method: POST
+            Param:  Major, Grade
+            Return: 400 if param missing
+                    200 if success
+         */
+        post("/api/v1/getcourse", ((request, response) -> {
             response.type("application/json");
             response.status(400);
-		    if (!request.queryParams().contains("Major") || !request.queryParams().contains("Grade"))
+            if (!request.queryParams().contains("Major") || !request.queryParams().contains("Grade"))
                 return new Gson().toJson("Missing parameter");
 
             response.status(200);
@@ -101,17 +70,17 @@ public class GoGauchoMain {
             return new Gson().toJson(courseArr);
         }));
 
-		/*
-		Post data to DB (internal testing only):
-		    Method: POST
-		    Param:  courseName, prerequisite, time, date, unit, uid
-		    Return: 400 if param missing
-		            200 if success
-		 */
-		post("/api/v1/postdata", (request, response) -> {
+        /*
+        Post data to DB (internal testing only):
+            Method: POST
+            Param:  courseName, prerequisite, time, date, unit, uid
+            Return: 400 if param missing
+                    200 if success
+         */
+        post("/api/v1/postdata", (request, response) -> {
             response.type("application/json");
             response.status(400);
-		    if (!request.queryParams().contains("courseName") || !request.queryParams().contains("prerequisite") ||
+            if (!request.queryParams().contains("courseName") || !request.queryParams().contains("prerequisite") ||
             !request.queryParams().contains("time") || !request.queryParams().contains("date") ||
             !request.queryParams().contains("unit") || !request.queryParams().contains("uid"))
                 return "Missing parameter";
@@ -126,24 +95,24 @@ public class GoGauchoMain {
             return c;
         });
 
-		/*
-		Login:
+        /*
+        Login:
             Method: POST
             Param:  userName, password
             Return: 400 if parameter missing
                     401 if login failed, ie userName or password incorrect
                     200 if login success
-		 */
-		post("/api/v1/login", ((request, response) -> {
-		    response.type("application/json");
+         */
+        post("/api/v1/login", ((request, response) -> {
+            response.type("application/json");
             response.status(400);
-		    if (!request.queryParams().contains("userName") || !request.queryParams().contains("password"))
+            if (!request.queryParams().contains("userName") || !request.queryParams().contains("password"))
                 return new Gson().toJson("Missing parameter");
             response.status(401);
             DocumentReference docRef = db.collection("users").document(request.queryParams("userName"));
             DocumentSnapshot document = docRef.get().get();
-		    if (!document.exists())
-		        return new Gson().toJson("Invalid user name or not registered");
+            if (!document.exists())
+                return new Gson().toJson("Invalid user name or not registered");
             if (!document.get("password").equals(request.queryParams("password")))
                 return new Gson().toJson("Incorrect password");
             response.status(200);
@@ -152,12 +121,19 @@ public class GoGauchoMain {
     }
     
     private static Firestore setupFirebase() throws Exception {
->>>>>>> e660f884501f8a4934033de6354c0521822e9af4
 
+        FileInputStream serviceAccount = new FileInputStream("src/main/java/edu/ucsb/cs56/gogaucho/ucsb-cs56-gogaucho-firebase-adminsdk-u3h8g-7d394cdd1c.json");
 
-		
-	}
-	
+        FirebaseOptions options = new FirebaseOptions.Builder()
+            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .setDatabaseUrl("https://ucsb-cs56-gogaucho.firebaseio.com")
+            .build();
+
+        FirebaseApp.initializeApp(options);
+        
+        return FirestoreClient.getFirestore();
+    }
+    
     private static int getHerokuAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
         if (processBuilder.environment().get("PORT") != null) {
@@ -165,6 +141,4 @@ public class GoGauchoMain {
         }
         return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
-
-	
 }
